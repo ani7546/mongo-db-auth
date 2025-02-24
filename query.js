4bff4467259a260bc6cb8b713f5e68f27edd3143
ani7546 <142499787+ani7546@users.noreply.github.com>
2025-02-24 10:51:02 +0530

    Update query.js

--- query.js
+++ query.js
@@ -1,3 +1,3 @@
-// query.js
-db.test_collection.insertOne({"name": "Test Document", "date": new Date()});
+use github_test;
+db.test_collection.insertOne({"name": "Test Document for github_test", "date": new Date()});
 print("Inserted document into test_collection");
